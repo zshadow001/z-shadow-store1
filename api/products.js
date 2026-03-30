12d4786ad8ac6902634d67b42ffb4646123c28bd
zshadow001 <parmarprince1982010@gmail.com>
2026-03-30 16:00:42 +0530

    Create products.js

---
+++ api/products.js
@@ -0,0 +1,36 @@
+export default function handler(req, res) {
+  res.status(200).json([
+    { name: "Telegram Account Banning", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Telegram channel banning", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Whatsapp unbanning", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Whatsapp banning", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Banning Methods", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Any Bomber", link: "https://t.me/Z_Shadow_x_talk_robot" },
+    { name: "Any apks", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "ip Hacking", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Insta Acc. Secure", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "How to use bins", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+
+    { name: "Story unban", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Unlimited gmail", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Snapchat Ban method", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Youtube ban method", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Whatsapp international number", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "OTP Bypass", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Phishing resoureses", link: "https://t.me/Z_Shadow_x_talk_robot" },
+    { name: "Website Info", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Network Or web hacking", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Show wifi password in cmd", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+
+    { name: "Rat for Android", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Child Po*n", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Dark web Access", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Carding in phone", link: "https://t.me/If_you_are_limited_dm_here_bot" },
+    { name: "Phishing Tools", link: "https://t.me/Z_Shadow_x_talk_robot" },
+    { name: "Website Making For business", link: "https://t.me/Z_Shadow_x_talk_robot" },
+    { name: "Logo designing", link: "https://t.me/Z_Shadow_x_talk_robot" },
+    { name: "Indian Best Telegram Id", link: "https://t.me/Z_Shadow_x_talk_robot" },
+    { name: "Camera Phishing", link: "https://t.me/Z_Shadow_x_talk_robot" },
+    { name: "Indian Otp panel", link: "https://t.me/Z_Shadow_x_talk_robot" }
+  ]);
+    }
